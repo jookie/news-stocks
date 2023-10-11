3e2ed756eaf8aed002590539a4a8e61a3913c8d9
Prerna Pandey <125113227+Prernanchan@users.noreply.github.com>
2023-10-11 12:38:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if ticker:
       
             html = BeautifulSoup(response, features="html.parser")
             news_table = html.find(id="news-table")
-            # news_tables[ticker] = news_table
+            news_tables[ticker] = news_table
 if news_table:
             parsed_data=[]
             for ticker, news_table in news_tables.items():
