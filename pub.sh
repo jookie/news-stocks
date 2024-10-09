b4075d5b07bac455177c1d096fe1cd571e998051
dovy <dovpeles@gmail.com>
2024-10-08 20:10:50 -0400

    Add file push.sh

---
+++ pub.sh
@@ -0,0 +1,25 @@
+# git init
+# export PATH=/usr/local/share/npm/bin:$PATH
+# git checkout -b my-new-branch
+#
+# git remote -v
+# git remote add origin https://github.com/jookie/jojoStock.git
+# git push --set-upstream origin main
+# git branch -M main
+git add .
+git commit -m "fix commit"
+git push -u origin main
+# -----------Go to GitHub and log in.
+# 1. Click on the "New" button to create a new repository.
+# 2. Name your repository and add a description.
+   # Do not initialize the repository with a README, .gitignore, or license if you've already initialized your project locally.
+# --------Remove Repository:
+# rm -rf .git
+# git init
+# If you've initialized a local repository and want to connect it to a remote repository on GitHub, GitLab, etc., follow these steps:
+## git remote add origin <remote_repository_URL>
+## 👇️ install yarn cli globally
+# npm install -g yarn
+
+
+
